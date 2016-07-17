a2afa8b139de3e9f9f6bf4e033b1a11910842139
yihoyoung <yihoyoung@nate.com>
2016-07-17 13:19:47 +0000

    add minitest

--- Gemfile
+++ Gemfile
@@ -31,6 +31,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard-minitest',     '2.3.1'
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
   get 'static_pages/help'
+  get 'static_pages/about'
 
   resources :microposts
   resources :users
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -60,5 +61,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  root 'users#index'
+  
 end
